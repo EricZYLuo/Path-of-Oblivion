b534459a201037def21968e3e0e8c764de965053
EricZYLuo <ericzyluo2002@gmail.com>
2021-01-05 15:58:40 -0500

    Started working on character class

--- character.cpp
+++ character.cpp
@@ -1,1 +1,27 @@
 #include "character.h"
+
+stats::stats(int stat[7], int alevel, int axp) {
+	for (int i = 0; i < 7; i++) {
+		bStats[i] = stat[i];
+	}
+	level = alevel;
+	xp = axp;
+	return;
+}
+void stats::checkLevel() {
+	double threshold = 100;
+	for (int i = 1; i < level; i++) {
+		threshold *= 1.05;
+	}
+	if (xp >= threshold) {
+		xp -= int(threshold);
+		level++;
+		//StatUpdate();
+	}
+	return;
+}
+
+void stats::statUpdate(int key, int change) {
+	bStats[key] += change;
+	return;
+}
--- character.h
+++ character.h
@@ -1,5 +1,25 @@
 #pragma once
-class character
-{
+
+class stats {
+	//Hp,Mp,atk,def,spA,spD,spe
+	int bStats[7];
+	int level;
+	int xp;
+
+public:
+	stats(int stat[7], int alevel, int axp);
+	void checkLevel();
+	void statUpdate(int key, int change);
+	void xpUpdate(int change);
+	int* getStats();
+	int getLevel();
+	int getXp();
+
 };
 
+class character
+{
+
+
+
+};
