43f286979f2b66ef1962a7ca8569cf7ae94cfc22
EricZYLuo <ericzyluo2002@gmail.com>
2021-01-07 15:53:08 -0500

    Finished header for items class

--- Items.h
+++ Items.h
@@ -4,7 +4,18 @@
 #include <string>
 
 class inventory {
-
+	int length;
+	Items* invList;
+public:
+	inventory();
+	inventory(int len, Items* list);
+	void invAdd(Items item);
+	void invDel(int pos);
+	void fullDel();
+	int findPos(std::string name);
+	Items findItem(int position);
+	int getLen();
+	
 };
 
 class Items
@@ -14,12 +25,26 @@ class Items
 
 public:
 	Items(std::string title, int val);
+	void changeName(std::string newName);
+	void changeVal(int change);
 	std::string getName();
 	int getValue();
 };
 
 class weapon:Items {
 	int atk;
-
+public:
+	weapon(std::string title, int val, int atk);
+	void changeAtk(int change);
+	int getAtk();
 };
 
+class equipment :Items {
+	int slot;
+	int modifiers[7];
+public:
+	equipment(std::string title, int val, int slot, int changes[7]);
+	void changeMods(int change[7]);
+	int getSlot();
+	int* getMods();
+ };
