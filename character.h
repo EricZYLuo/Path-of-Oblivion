56f11df34c1d4f5d5d8acefab7bf47b7d3a4bb58
EricZYLuo <ericzyluo2002@gmail.com>
2021-01-19 23:55:55 -0500

    Started working character header file

--- character.h
+++ character.h
@@ -5,11 +5,24 @@
 #include <fstream>
 #include <string>
 #include "stats.h"
+#include "inventory.h"
+#include "Items.h"
+#include "MapFiles.h"
 
 class character
 {
 	std::string name;
 	stats charStats;
-	//Inventory
+	int gold;
+	inventory Inv;
+	posn playerPos;
+
+public:
+	character();
+	character(std::string fileName);
+	void battle(character enemy);
+	void changeName(std::string newName);
+	void setStats(stats newStats);
+	void singleStatsChange(int key, int change);
 };
 
